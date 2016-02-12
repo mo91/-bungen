c99009b356739f35a3cc26903272af79086ddd8f
Marcus Oberdieck <marcus.oberdieck@unique-solutions.eu>
2016-02-12 14:10:36 +0100

    Erste Übung

---
+++ jks übungen.exe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jks_übungen.exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt.");
+            Console.ReadLine();
+        }
+    }
+}
